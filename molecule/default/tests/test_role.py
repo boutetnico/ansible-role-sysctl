ff24c82ac60ef975523dbbbeeef31c4c89e647b6
Nicolas Boutet <amd3002@gmail.com>
2023-04-26 11:37:39 +0200

    Adapt CI workflow for molecule 5 (#3)
    * Adapt CI workflow for molecule 5
    
    * Add pre-commit
    
    * Let ansible-lint run yamllint
    
    * Fix ansible-lint violation
    
    * Remove flake8

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,12 +5,16 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('key,expected_value', [
-  ('vm.overcommit_memory', 1),
-])
+@pytest.mark.parametrize(
+    "key,expected_value",
+    [
+        ("vm.overcommit_memory", 1),
+    ],
+)
 def test_sysctl_values(host, key, expected_value):
     value = host.sysctl(key)
     assert expected_value == value
