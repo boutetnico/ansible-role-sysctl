2dcec30209a49af1bc44229999d310c68cd411d8
Nicolas Boutet <amd3002@gmail.com>
2026-02-10 08:33:42 +0100

    Improve test coverage

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -4,9 +4,22 @@ import os
 
 
 @pytest.mark.parametrize(
+    "package",
+    [
+        "procps",
+    ],
+)
+def test_dependencies_installed(host, package):
+    pkg = host.package(package)
+    assert pkg.is_installed
+
+
+@pytest.mark.parametrize(
     "key,expected_value",
     [
         ("vm.overcommit_memory", 1),
+        ("vm.swappiness", 60),
+        ("net.ipv4.ip_forward", 0),
     ],
 )
 def test_sysctl_values(host, key, expected_value):
